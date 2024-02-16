ce2011a5805888444d074cbdb0802c7fac665693
sndpjaat <sndpjaat008@gmail.com>
2024-02-16 10:15:04 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        'DM Sans': ('DM Sans'),
+      }
+    },
+  },
+  plugins: [],
+}
+
